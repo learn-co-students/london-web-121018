68f75d9f981ea7ad2f53bf53489f5ad160d5d01e
danielkaczmarczyk <danielkaczmarczyk2@gmail.com>
2019-01-21 16:00:22 +0000

    edits

--- 16-rails-sessions/docoffice/config/routes.rb
+++ 16-rails-sessions/docoffice/config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # resources :doctors, only: [:index, :show, :new, :create, :edit, :update, :delete]
   resources :doctors
   resources :pills, only: [:new, :show, :create]
+
+  get '/clean', to: 'doctors#clean'
+  
 end
